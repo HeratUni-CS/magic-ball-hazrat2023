7bc3660de75ae0034f5a0a95b2605de32de2c866
ghulam hazrat <hazrat.sultani@gmail.com>
2023-05-02 11:45:59 -0700

    creating magic ball in dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+    MaterialApp(
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("Ask me any question please"),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 4;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          print("i got clicked");
+
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
 }
